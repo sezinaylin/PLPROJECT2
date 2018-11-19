9f9936a74187c41fcea58e6a54553a030881c4aa
Aylin Sezin Açıkgöz <sezinaylin1@gmail.com>
2018-11-19 15:24:58 +0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,22 @@
+﻿create= open("text.txt","w")
+
+while True:
+   sentence = input("enter the sentence :")
+   word=""
+   for i in range(len(sentence)):
+      if sentence[i:i+3]=="...":
+         break
+      #elif sentence[i]==".":
+         #break
+      word = word + sentence[i:i+1]
+   create.write(word)
+   if "..." in sentence:
+      break
+  # elif "." in sentence:
+  #    break
+
+
+create.close()
+st1 = open("text.txt").read()
+print("text: "+st1)
+create.close()
